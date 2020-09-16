f5be9686da01efa6427ae76b893cfa7ad4199c70
Arthur Carson <rthrcrsn@gmail.com>
2020-09-15 20:49:15 -0500

    Added jxm code to files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return(
+            <div>
+                <p>&#169 React Actors</p>
+            </div>
+        )
+    }
+}
